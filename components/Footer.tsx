3cac5304401192d0a15ba071d39d7512d666c97d
Aditya-Srivastava <pks85715@gmail.com>
2024-10-12 21:27:29 +0530

    footer file added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,62 +8,47 @@ const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
       {/* background grid */}
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <img
-          src="/footer-grid.svg"
-          alt="grid"
-          className="w-full h-full opacity-50 "
-        />
-      </div>
-
-      <div className="flex flex-col items-center">
-        <h1 className="heading lg:max-w-[45vw]">
-          Ready to take the <span className="text-purple">next step</span> to build the imagination into reality
-        </h1>
-        <p className="text-white-200 md:mt-10 my-5 text-center">
-          Reach out to us and let&apos;s discuss how we can help you
-          achieve your goals.
-        </p>
-        <a href="mailto:this@this.com">
-          <MagicButton
-            title="Let's get in touch"
-            icon={<FaLocationArrow />}
-            position="right"
+      <Vortex backgroundColor="transparent">
+        <div className="w-full absolute left-0 -bottom-72 min-h-96">
+          <img
+            src="/footer-grid.svg"
+            alt="grid"
+            className="w-full h-full opacity-50 "
           />
-        </a>
-      </div>
-      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
-        <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © Signodes Club , NIET
-        </p>
-
-        <div className="flex items-center md:gap-3 gap-6">
-          {socialMedia.map((info) => (
-            <div
-              key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
-            >
-              <img src={info.img} alt="icons" width={20} height={20} />
-            </div>
-          ))}
         </div>
-      </div>
-      <Vortex
-        backgroundColor="black"
-        className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
-      >
-        <h2 className="text-white text-2xl md:text-6xl font-bold text-center">
-          The hell is this?
-        </h2>
-        <p className="text-white text-sm md:text-2xl max-w-xl mt-6 text-center">
-          This is chemical burn. It&apos;ll hurt more than you&apos;ve ever been
-          burned and you&apos;ll have a scar.
-        </p>
-        <div className="flex flex-col sm:flex-row items-center gap-4 mt-6">
-          <button className="px-4 py-2 bg-blue-600 hover:bg-blue-700 transition duration-200 rounded-lg text-white shadow-[0px_2px_0px_0px_#FFFFFF40_inset]">
-            Order now
-          </button>
-          <button className="px-4 py-2  text-white ">Watch trailer</button>
+
+        <div className="flex flex-col items-center">
+          <h1 className="heading lg:max-w-[45vw]">
+            Ready to take the <span className="text-purple">next step</span> to
+            build the imagination into reality
+          </h1>
+          <p className="text-white-200 md:mt-10 my-5 text-center">
+            Reach out to us and let&apos;s discuss how I can help you achieve
+            your goals.
+          </p>
+          <a href="mailto:contact@jsmastery.pro">
+            <MagicButton
+              title="Let's get in touch"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
+        </div>
+        <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+          <p className="md:text-base text-sm md:font-normal font-light">
+            Copyright © Signodes Club , NIET
+          </p>
+
+          <div className="flex items-center md:gap-3 gap-6">
+            {socialMedia.map((info) => (
+              <div
+                key={info.id}
+                className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+              >
+                <img src={info.img} alt="icons" width={20} height={20} />
+              </div>
+            ))}
+          </div>
         </div>
       </Vortex>
     </footer>
