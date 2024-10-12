171a37e7f50123539655273dbceb6d182694f30c
Aditya-Srivastava <pks85715@gmail.com>
2024-10-12 20:36:36 +0530

    Nextjs file added

---
+++ data/index.ts
@@ -0,0 +1,259 @@
+export const navItems = [
+  {name: "Home" , link: "#home"},
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Members", link: "#members" },
+  { name: "Contact", link: "#contact" },
+  {name: "Events" , link: "#events"},
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "Signodes is a IoT based Club",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/grid.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "/grid.svg",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    // /b4.svg
+    spareImg: "", 
+  },
+
+  {
+    id: 5,
+    title: "",
+    description: "",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "",
+    des: "",
+    img: "/grid.svg",
+    // iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+    pintitle : "EzeAgro",
+  },
+  {
+    id: 2,
+    title: "",
+    des: "",
+    img: "/grid.svg",
+    // iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+    pintitle : "Robot",
+  },
+  {
+    id: 3,
+    title: "",
+    des: "",
+    img: "/grid.svg",
+    // iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+    pintitle : "EzeBuddies",
+  },
+  {
+    id: 4,
+    title: "",
+    des: "",
+    img: "/grid.svg",
+    // iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+    pintitle : "Smart street lights",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Mayank Deep Khare",
+    desc: "this is person",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Addite Matttoo",
+    desc: "this is person",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 3,
+    title: "Prashant Kumar Signh",
+    desc: "this is person",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 4,
+    title: "Amiya Krishan Gupta",
+    desc: "this is person",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 5,
+    title: "Arpit Raj Barnwal",
+    desc: "this is person",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 6,
+    title: "Piyush Kumar Jha",
+    desc: "this is person",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 7,
+    title: "Aryan Srivastav",
+    desc: "this is person",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 8,
+    title: "Sumesh Sarkar",
+    desc: "this is person",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 9,
+    title: "Rishi Ranjan",
+    desc: "this is person",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
