0b4681dbaf0f27c6dbcc318554d85eeaab2740c2
Aditya-Srivastava <pks85715@gmail.com>
2024-10-12 23:19:54 +0530

    title added

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "Signodes club , NIET",
+  description: "Landing page of signodes club",
 };
 
 export default function RootLayout({
